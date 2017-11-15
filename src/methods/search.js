a657d952ba216e66e899072edb1dbaf313f48276
thekeeb <33702517+thekeeb@users.noreply.github.com>
2017-11-15 14:26:29 -0800

    Update search.js

--- src/methods/search.js
+++ src/methods/search.js
@@ -1,7 +1,5 @@
 import spotifyApi from '../api/spotify';
 
-console.log('at least weve made it to the search 1.');
-
 const search = res => async query => {
   console.log('at least weve made it to the search 2.');
   try {
@@ -9,8 +7,10 @@ const search = res => async query => {
     const { body: { tracks: { items: tracks } } } = await spotifyApi.searchTracks(query, {limit: 10});
     const messages = tracks.reduce((acc, val, ind) => {
       const artists = val.artists.reduce((acc, val, ind) => `${acc}${ind !== 0 ? ',' : ''} ${val.name}`, '')
+      console.log('artists:', `${acc}${artists} - ${val.name}\n`);
       return `${acc}${artists} - ${val.name}\n`;
     }, 'One of these? \n');
+    console.log('messages', messages);
     // const payload = {
     //   attachments: [
     //     {
